80d041cb2da4e682746611c7b6d2c830169ea6e2
rmsehd <rmseh@DESKTOP-TQB2GT4>
2021-04-01 15:35:26 +0900

    로그아웃 수정

--- src/main/java/com/app/project/user/rest/UserRestController.java
+++ src/main/java/com/app/project/user/rest/UserRestController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.app.project.user.UserService;
 
